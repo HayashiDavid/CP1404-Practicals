68eb4cb0009d34a1c6dd473bd931d1bdc28c1fb9
CYBORGE <thunyantun24crown@gmail.com>
2025-05-31 13:57:51 +0800

    Getting scores with functions

---
+++ prac_02/score.py
@@ -0,0 +1,26 @@
+import random
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(get_score(score))
+
+    random_score = random.randint(0,100)
+    print(f"Random score: {random_score:.2f}")
+    print(get_score(random_score))
+
+def get_score(score):
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
