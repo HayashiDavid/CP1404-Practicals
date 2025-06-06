11cb0a2ced86d4c4bda92c6c1fe7ac973974482b
CYBORGE <thunyantun24crown@gmail.com>
2025-06-06 15:19:38 +0800

    Only normal letters check and special characters required

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -44,8 +44,12 @@ def is_valid_password(password):
         elif character in SPECIAL_CHARACTERS:
             number_of_special += 1
 
-    
-    # and return False if it's zero
+
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
+
+    if IS_SPECIAL_CHARACTER_REQUIRED or number_of_special == 0:
+        return False
 
     # if we get here (without returning False), then the password must be valid
     return True
