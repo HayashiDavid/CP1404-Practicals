d016d0f8971f4e409bdf3a5660f398b1eb7ab6e0
CYBORGE <thunyantun24crown@gmail.com>
2025-06-06 13:35:18 +0800

    Randoms answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+"""
+What did you see on line 1?
+smallest number is 5
+largest number is 20
+
+What did you see on line 2?
+smallest number is 5
+largest number is 9
+
+Could line 2 have produced a 4?
+Line 2 could not have produced 4
+
+What did you see on line 3?
+smallest number = 2.5
+largest number = 5.5
+"""
+import random
+print(random.randint(1, 100))
