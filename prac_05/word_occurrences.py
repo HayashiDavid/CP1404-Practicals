48a16651db4b4d8ed826723a67d34932cc5900c9
CYBORGE <thunyantun24crown@gmail.com>
2025-06-21 17:48:43 +0800

    Create functions that find the max width and print the results

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,11 +3,10 @@ Estimate: 1 hour
 Actual: 35 minutes
 """
 def main():
-    words_to_count = {}
-    lengths = []
-
     text = input("Text>>> ")
     words_to_count = count_words(text)
+    sorted_words = sort_words(words_to_count)
+    print_results(words_to_count, sorted_words)
 
 def count_words(text):
     """ Count words and return to dictionary """
@@ -24,4 +23,16 @@ def count_words(text):
 
 def sort_words(words_to_count):
     """ Sort words alphabetically """
-    return  sorted(words_to_count.key())
+    return  sorted(words_to_count.keys())
+
+def find_max_width(sorted_words):
+    """ Find the longest word for formatting """
+    return max(len(phrases) for phrases in sorted_words)
+
+def print_results(words_to_count, sorted_words):
+    """ Print results with aligned formatting """
+    max_width = find_max_width(sorted_words)
+    for text in sorted_words:
+        print(f"{text:{max_width}} : {words_to_count[text]}")
+
+main()
