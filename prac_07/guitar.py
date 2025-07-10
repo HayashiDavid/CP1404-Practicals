920e7a36dd6480cc0e5a9c2943c6a575ebab6c3b
CYBORGE <thunyantun24crown@gmail.com>
2025-07-10 11:28:02 +0800

    Remove the if statement in is_vintage function to make it simplier

---
+++ prac_07/guitar.py
@@ -0,0 +1,22 @@
+CURRENT_TIME = 2025
+VINTAGE_YEAR = 50
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Function for the classes"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return the string"""
+        return f"{self.name} ({self.year}): ${self.cost:,.2f}"
+
+    def get_age(self):
+        """This function allows it to calculate to get the age"""
+        age = CURRENT_TIME - self.year
+        return age
+
+    def is_vintage(self):
+        """Check if vintage or not"""
+        return self.get_age() >= VINTAGE_YEAR
