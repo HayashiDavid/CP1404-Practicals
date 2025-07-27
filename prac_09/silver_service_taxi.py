d65137e9e1b38e0802a27fb77b0a61fe34e29d7a
CYBORGE <thunyantun24crown@gmail.com>
2025-07-27 17:35:33 +0800

    create str and get fare function

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,3 +9,11 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return string representation"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Get current fare"""
+        return self.flagfall + super().get_fare()
