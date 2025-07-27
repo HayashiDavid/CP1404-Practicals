226de4e168d53ad6bc57956001ac642f2915272c
CYBORGE <thunyantun24crown@gmail.com>
2025-07-27 14:29:58 +0800

    create str function

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -14,6 +14,9 @@ class Taxi(Car):
         self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
         """Return the price for the taxi trip."""
