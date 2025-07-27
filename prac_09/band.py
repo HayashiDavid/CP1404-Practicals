90ff6e1d769fc71335173f75b4a61e7e27e163fd
CYBORGE <thunyantun24crown@gmail.com>
2025-07-27 15:26:29 +0800

    Create add function to add musicians and str and repr to return string

--- prac_09/band.py
+++ prac_09/band.py
@@ -4,3 +4,15 @@ class Band:
         self.band = band
         self.musicians = []
 
+    def __str__(self):
+        """Return string representation"""
+        return f"{self.band}({self.musicians})"
+
+    def __repr__(self):
+        """Return a string"""
+        return str(self)
+
+    def add(self, musician):
+        """Add musician"""
+        return self.musicians.append(musician)
+
